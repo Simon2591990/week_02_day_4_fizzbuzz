cc4ba290e1362b606d1dae544b26abdc63f9283e
Simon Charters <simon259@hotmail.co.uk>
2020-11-26 11:40:25 +0000

    tested fizz buzz

--- tests/fizzbuzz_test.py
+++ tests/fizzbuzz_test.py
@@ -7,3 +7,6 @@ class TestFizzBuzz(unittest.TestCase):
 
     def test_fizzbuzz__5_returns_buzz(self):
         self.assertEqual("Buzz", fizzbuzz(5))
+
+    def test_fizzbuzz__15_returns_fizzbuzz(self):
+        self.assertEqual("Fizz Buzz", fizzbuzz(15))
