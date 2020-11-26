85034d8d52464a8c532b4829eb35f3bf412f4a18
Simon Charters <simon259@hotmail.co.uk>
2020-11-26 11:39:14 +0000

    tested buzz

--- src/fizzbuzz.py
+++ src/fizzbuzz.py
@@ -1,6 +1,8 @@
 def fizzbuzz(number):
     if number % 3 == 0:
         return "Fizz"
+    elif number % 5 == 0:
+        return "Buzz"
     
 
 
--- tests/fizzbuzz_test.py
+++ tests/fizzbuzz_test.py
@@ -4,3 +4,6 @@ from src.fizzbuzz import fizzbuzz
 class TestFizzBuzz(unittest.TestCase):
     def test_fizzbuzz__3_returns_fizz(self):
         self.assertEqual("Fizz", fizzbuzz(3))
+
+    def test_fizzbuzz__5_returns_buzz(self):
+        self.assertEqual("Buzz", fizzbuzz(5))
